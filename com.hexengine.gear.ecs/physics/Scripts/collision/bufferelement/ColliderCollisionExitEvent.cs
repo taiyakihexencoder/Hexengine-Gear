50bcf2995f3058a21e6b153437ba0c402bb5f699
taiyakihexencoder <nameswithb@gmail.com>
2025-11-09 23:09:24 +0900

    Collision, Triggerを作成

---
+++ com.hexengine.gear.ecs/physics/Scripts/collision/bufferelement/ColliderCollisionExitEvent.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+
+namespace com.hexengine.gear.ecs {
+	public struct ColliderCollisionExitEvent : IBufferElementData {
+		public Entity Self { get; private set; }
+		public Entity Other { get; private set; }
+		public int SelfIndex { get; private set; }
+		public int OtherIndex { get; private set; }
+		public ColliderKey SelfColliderKey { get; private set; }
+		public ColliderKey OtherColliderKey { get; private set; }
+		public float3 Normal { get; private set; }
+
+		public ColliderCollisionExitEvent(ColliderCollisionEvent evt, Entity other) {
+			if (other == evt.EntityA) {
+				Self = evt.EntityB;
+				SelfIndex = evt.BodyIndexB;
+				SelfColliderKey = evt.ColliderKeyB;
+				Other = evt.EntityA;
+				OtherIndex = evt.BodyIndexA;
+				OtherColliderKey = evt.ColliderKeyA;
+				Normal = -evt.Normal;
+			} else {
+				Self = evt.EntityA;
+				SelfIndex = evt.BodyIndexA;
+				SelfColliderKey = evt.ColliderKeyA;
+				Other = evt.EntityB;
+				OtherIndex = evt.BodyIndexB;
+				OtherColliderKey = evt.ColliderKeyB;
+				Normal = evt.Normal;
+			}
+		}
+	}
+}
---
+++ com.hexengine.gear.ecs/physics/Scripts/trigger/bufferelement/ColliderTriggerEnterEvent.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Entities;
+using Unity.Physics;
+
+namespace com.hexengine.gear.ecs {
+	public struct ColliderTriggerEnterEvent : IBufferElementData {
+		public Entity Self { get; private set; }
+		public Entity Other { get; private set; }
+		public int SelfIndex { get; private set; }
+		public int OtherIndex { get; private set; }
+		public ColliderKey SelfColliderKey { get; private set; }
+		public ColliderKey OtherColliderKey { get; private set; }
+
+		public ColliderTriggerEnterEvent(ColliderTriggerEvent evt, Entity other) {
+			if (other == evt.EntityA)
+			{
+				Self = evt.EntityB;
+				SelfIndex = evt.BodyIndexB;
+				SelfColliderKey = evt.ColliderKeyB;
+				Other = evt.EntityA;
+				OtherIndex = evt.BodyIndexA;
+				OtherColliderKey = evt.ColliderKeyA;
+			}
+			else
+			{
+				Self = evt.EntityA;
+				SelfIndex = evt.BodyIndexA;
+				SelfColliderKey = evt.ColliderKeyA;
+				Other = evt.EntityB;
+				OtherIndex = evt.BodyIndexB;
+				OtherColliderKey = evt.ColliderKeyB;
+			}
+		}
+	}
+}
