0715ed5622181b6d0d3b2ec3f46b9906320e0295
taiyakihexencoder <nameswithb@gmail.com>
2025-11-09 23:08:48 +0900

    汎用削除処理

---
+++ com.hexengine.gear.ecs/core/Scripts/job/DestroyEntitiesJob.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+
+namespace com.hexengine.gear.ecs {
+	public partial struct DestroyEntitiesJob : IJobEntity {
+		public EntityCommandBuffer commandBuffer;
+		void Execute(in Entity entity) {
+			commandBuffer.DestroyEntity(entity);
+		}
+	}
+
+	public partial struct DestroyEntitiesParallelJob : IJobEntity {
+		public EntityCommandBuffer.ParallelWriter commandBuffer;
+		void Execute(in Entity entity, [EntityIndexInQuery] int sortKey) {
+			commandBuffer.DestroyEntity(sortKey, entity);
+		}
+	}
+}
